41b9894b275c2f5d43b60761757a40d9a1750838
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2025-06-14 17:39:56 +0300

    Add URL opening functionality to MaterialCard for specific book IDs

--- lib/view/home/material/widget/materail_Card.dart
+++ lib/view/home/material/widget/materail_Card.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:l_sha_a/core/constatnt.dart';
+import 'package:l_sha_a/core/function.dart';
 import 'package:l_sha_a/data/models/material_model.dart';
 
 class MaterialCard extends StatelessWidget {
@@ -11,7 +15,36 @@ class MaterialCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    log("Book ID: ${book.id}");
+    log("mater ID: ${Constatnt.cuurentMaterial}");
     return GestureDetector(
+      onTap: () {
+        if (Constatnt.cuurentMaterial == "اقتصاد كلي" && book.id == "10_MAC") {
+          openUrl(
+              "https://drive.google.com/file/d/1MswQ9fkHHuXwYxHITMkN_ts7-KT_d8tF/view?usp=drive_link");
+        }
+        if (Constatnt.cuurentMaterial == "اقتثاد جزئي" &&
+            book.id == "19_ME_2000") {
+          openUrl(
+              "https://drive.google.com/file/d/1YIrQdgYmKq5d6HxLGj5iuZh_9-F9a2D6/view?usp=drive_link");
+        } else if (Constatnt.cuurentMaterial == "الاحصاء" &&
+            book.id == "10_S 310_م.م 2009") {
+          openUrl(
+              "https://drive.google.com/file/d/1PuvOz0be5ffh6b3ZqBcsdvJl6-W5jjoi/view?usp=drive_link");
+        } else if (Constatnt.cuurentMaterial == "التسويق" &&
+            book.id == "15_M 658,8_D.G") {
+          openUrl(
+              "https://drive.google.com/file/d/11UV01H__7IoFN0uPlDDRHboUvaUlOLz9/view?usp=drive_link");
+        } else if (Constatnt.cuurentMaterial == "السلوك التنظيمي" &&
+            book.id == "350_AS_2020") {
+          openUrl(
+              "https://drive.google.com/file/d/1MvCqNFRyoGskf9uOVlQfgQzLep7bnoP9/view?usp=drive_link");
+        } else if (Constatnt.cuurentMaterial == "ادارة الموارد البشرية" &&
+            book.id == "15_H.R.M 350_P.R 2019 1") {
+          openUrl(
+              "https://drive.google.com/file/d/1PuvOz0be5ffh6b3ZqBcsdvJl6-W5jjoi/view?usp=drive_link");
+        }
+      },
       child: Card(
         child: ListTile(
           title: Align(
